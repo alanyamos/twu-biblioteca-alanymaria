102e4edb9786622f299ace71a83e271bfa8c8c2f
Alany Maria <lamariasilva@MacBook-Pro-de-Alany.local>
2019-09-24 10:12:18 -0300

    1.1: implementing a new class Biblioteca that print in the console a welcome message when the app starts

--- src/com/twu/biblioteca/Biblioteca.java
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -1,4 +1,17 @@
 package com.twu.biblioteca;
 
+import java.io.PrintStream;
+
 public class Biblioteca {
+
+    private PrintStream printStream;
+
+    public Biblioteca(PrintStream printStream) {
+        this.printStream = printStream;
+    }
+
+    public void printWelcomeMessage() {
+        String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        printStream.println(welcomeMessage);
+    }
 }
--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -1,13 +1,32 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
-public class ExampleTest {
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class BibliotecaTest {
+
+    private PrintStream printStream;
+    private Biblioteca biblioteca;
+
+    @Before
+    public void setUp() {
+        printStream = mock(PrintStream.class);
+        biblioteca = new Biblioteca(printStream);
+    }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void shouldPrintAWelcomeMessageWhenApplicationStarts() {
+        String expectedMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        biblioteca.printWelcomeMessage();
+        verify(printStream).println(expectedMessage);
     }
 }
+
